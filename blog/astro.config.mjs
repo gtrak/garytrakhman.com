536eb18e825a698a2f47dd5b2422ac02368224f5
Gary Trakhman <gary.trakhman@gmail.com>
2023-02-22 08:01:07 -0500

    Add sitemap

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -4,14 +4,12 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://blog.garytrakhman.com",
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind(),
-    partytown({
-      config: { forward: ["dataLayer.push"] },
-    }),
-  ],
+  integrations: [mdx(), sitemap(), tailwind(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  })]
 });
